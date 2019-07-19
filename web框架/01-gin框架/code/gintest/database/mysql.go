b2eb3dce13400a3b8aa57d3f2b7bb9dc72ed906b
jerry <jerry@gmail.com>
2019-07-19 13:50:40 +0800

    no message

---
+++ web框架/01-gin框架/code/gintest/database/mysql.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var SqlDB *sql.DB
+
+func init() {
+	var err error
+	SqlDB, err = sql.Open("mysql", "root:a123456@tcp(x.x.x.x:3306)/gintestdb?parseTime=true")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	err = SqlDB.Ping()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+}
